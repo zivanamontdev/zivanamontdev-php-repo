671c40d14fb4ceeaaa4c683ad54d31f098b0d3c8
aran-md <andirifqialnur276@gmail.com>
2026-01-04 01:40:00 +0800

    broo i swear if this wont work youll gonna fill my wrath

--- app/views/components/widget/activities/activities_gallery_card.php
+++ app/views/components/widget/activities/activities_gallery_card.php
@@ -85,16 +85,12 @@ if (!function_exists('getActivityGalleryImageUrl')) {
             </div>
             
             <!-- Image -->
-            <div class="w-full h-[158px] md:h-[626px] rounded-[12px] overflow-hidden mb-[36px] bg-gray-placeholder relative">
+            <div class="w-full h-[158px] md:h-[626px] rounded-[12px] overflow-hidden mb-[36px]">
                 <img 
                     :src="currentImage" 
                     alt="" 
                     class="w-full h-full object-cover"
-                    @error="$event.target.style.display='none'; $event.target.nextElementSibling.classList.remove('hidden');"
                 >
-                <svg class="hidden absolute inset-0 w-16 h-16 m-auto text-white-shadow" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
-                </svg>
             </div>
             
             <!-- Description -->
